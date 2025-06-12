3d679033743f208940b7f7656515da5122d5ffe8
Vivi <golovinad84@gmail.com>
2025-06-12 18:37:39 +0300

    fixed CodeStyle in CommentMapper

--- src/main/java/ru/practicum/shareit/item/CommentMapper.java
+++ src/main/java/ru/practicum/shareit/item/CommentMapper.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.UserMapper;
